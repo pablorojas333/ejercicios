a89bb59da8c1fa02a5ff222362ec366dd38bea33
puntodigital <137305159+pablorojas333@users.noreply.github.com>
2023-08-03 11:49:36 -0300

    Update ejercioenteroyreales.py

--- ejercioenteroyreales.py
+++ ejercioenteroyreales.py
@@ -5,5 +5,6 @@ while (c<0) and (i<=0) or (i>=100) and (m<=0):
     c=float(input('Introduce el capital: '))
     i=int(input('Introduce el interes: '))
     m=int(input('Introduce el tiempo en años: '))
-
+for j in range (m):
+    c= c*(1+i/100)
 print('tienes ', c, 'pesos')
